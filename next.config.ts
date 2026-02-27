f8fbdc949c1038bbea831854e5e6269437d221b1
bit913srw <sydneyrweiss@gmail.com>
2026-02-27 00:47:54 +0000

    Fix next config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
-import path from 'path';
 import { NextConfig } from 'next';
 
 const config: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (cfg) => {
-    if (cfg.resolve) {
-      cfg.resolve.alias = {
-        ...(cfg.resolve.alias || {}),
-        '@': path.resolve(__dirname),
-      };
-    }
-    return cfg;
-  },
+  turbopack: {}
 };
 
 export default config;
